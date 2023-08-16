cdcea0b16dde7a58e415449680e7068fdc5ae4c6
henilcioterras <henilcioterras@gmail.com>
2023-08-16 21:47:46 +0000

    Nova Atualizacao funcional

---
+++ app/controllers/ProductController.php
@@ -0,0 +1,17 @@
+<?php
+namespace app\controllers;
+
+    class ProductController{
+        
+        public function index(){
+            var_dump('index product');
+        }
+        
+        public function update(){
+            var_dump('update product');
+        }
+        
+        
+    }
+
+?>
